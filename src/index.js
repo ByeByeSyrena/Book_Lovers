2ee7c2e4b344ba39b15df74c3ee0d33884322f9a
Ira S <iryna.shevchenko2@gmail.com>
2023-08-30 15:27:45 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/add-book-modal';
+import './js/authorization-page';
+import './js/header';
+import './js/home';
+import './js/menu';
+import './js/remove-book-modal';
+import './js/scroll';
+import './js/shopping-list';
+
