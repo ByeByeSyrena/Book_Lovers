a60cf87e455956635f92a4275064bd8b5163da7f
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2023-09-01 12:41:15 +0300

    add markups update categories

---
+++ src/js/markup-category.js
@@ -0,0 +1,8 @@
+const booksContainerEl = document.querySelector(".main-books-container")
+
+export default function markupByCategory(bookData) {
+    // console.log(bookData);
+    // АННА побудувати html ждя одної книжки 
+    // вставити в родітєля(booksContainerEl)
+   
+}
