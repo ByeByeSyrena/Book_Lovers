434493791c4c11e4902fa3e96c90b2b72fc31303
Ira S <iryna.shevchenko2@gmail.com>
2023-09-03 01:10:53 +0300

    меню скролл через js, 2 години знущань над js)

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,10 @@
 const headerIcons = document.querySelector(".header-mobile-menu-icons");
-
+const menu = document.getElementById("mobile-menu-container").classList;
    
 headerIcons.addEventListener("click", () => {
     const iconMenu = document.getElementById("open-mobile-menu").classList;
     const iconCross = document.getElementById("close-mobile-menu").classList;
     
-    const menu = document.getElementById("mobile-menu-container").classList;
-
 
         if (iconMenu.contains("burger-menu")) {
             iconMenu.replace("burger-menu", "hidden");
@@ -25,6 +23,19 @@ headerIcons.addEventListener("click", () => {
     };
 });
 
+
+window.addEventListener('scroll', () => {
+const allowedScrollPercentage = 10;
+
+const viewportHeight = window.innerHeight;
+    const allowedScrollPixels = (allowedScrollPercentage / 100) * viewportHeight;
+    
+  const scrollY = window.scrollY;
+
+  if (menu.contains("mobile-menu-container") && scrollY > allowedScrollPixels) {
+    window.scrollTo(0, allowedScrollPixels);
+  }
+});
 
 
     
