ccd211f38d7a4e155880c3d470227771f73f86cc
Andrey <andrey291188@gmail.com>
2023-09-04 22:19:25 +0300

    up

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,156 +1,121 @@
-import { creatMarkup } from './markup-shopping-list';
+import { bookArray, setMarkup } from './markup-shopping-list';
 
 const BOOKS_CARDS = 'bookCards';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const pagination = document.querySelector('.pagination');
-  const quantityCards = document.querySelector('.shopping__list__list');
-  const pageList = document.querySelector('.pagination-list');
-  const pageItem = document.querySelectorAll('.page-item');
-  const lastPage = document.querySelector('.last-page');
-  const nextPage = document.querySelector('.next-page');
-  const prevPage = document.querySelector('.prev-page');
-  const firstPage = document.querySelector('.first-page');
-  const numbers = document.querySelector('.numbers');
+const pagination = document.querySelector('.pagination');
+const nextPage = document.querySelector('.next-page');
+const prevPage = document.querySelector('.prev-page');
+const firstPage = document.querySelector('.first-page');
+const lastPage = document.querySelector('.last-page');
+const numbersPage = document.querySelector('.numbers');
 
-  const perPage = window.innerWidth >= 768 ? 3 : 4;
-  let bookArray = JSON.parse(localStorage.getItem(BOOKS_CARDS));
-  let currentPage = 1;
-  
+pagination.addEventListener('click', onClick);
+numbersPage.addEventListener('click', onActiveBtn);
 
-  function getPages(totalPages, currentPage) {
-    const maxLength = window.innerWidth >= 768 ? 7 : 9;
-    const visiblePages = [];
+let perPage = null;
+let totalPages = 0;
+let pageNumber = 1;
 
-    if (totalPages <= maxLength) {
-      for (let i = 1; i <= totalPages; i++) {
-        visiblePages.push(i);
-      }
-    } else {
-      if (currentPage <= maxLength - 2) {
-        for (let i = 1; i <= maxLength - 2; i++) {
-          visiblePages.push(i);
-        }
-        visiblePages.push('...');
-        visiblePages.push(totalPages);
-      } else if (currentPage >= totalPages - maxLength + 3) {
-        visiblePages.push(1);
-        visiblePages.push('...');
-        for (let i = totalPages - maxLength + 3; i <= totalPages; i++) {
-          visiblePages.push(i);
-        }
-      } else {
-        visiblePages.push(1);
-        visiblePages.push('...');
-        for (
-          let i = currentPage - Math.floor(maxLength / 2);
-          i <= currentPage + Math.floor(maxLength / 2);
-          i++
-        ) {
-          visiblePages.push(i);
-        }
-        visiblePages.push('...');
-        visiblePages.push(totalPages);
-      }
-    }
-    return visiblePages;
+export function handleViewportResize() {
+  const viewportWidth =
+    window.innerWidth || document.documentElement.clientWidth;
+  if (viewportWidth < 767) {
+    perPage = 4;
+  } else {
+    perPage = 3;
   }
 
-  function updatePagination() {
-    pageList
-      .querySelectorAll('.page-item.current-page, .page-item.dots')
-      .forEach(item => item.remove());
-
-    numbers.innerHTML = '';
-
-    const totalPages = Math.ceil(bookArray.length / perPage);
-    const visiblePages = getPages(totalPages, currentPage);
-
-    visiblePages.forEach(item => {
-      const pageItemElement = document.createElement('li');
-      pageItemElement.classList.add('page-item');
-      pageItemElement.classList.add(
-        item === currentPage ? 'current-page' : 'dots'
-      );
-
-      const pageLinkElement = document.createElement('a');
-      pageLinkElement.classList.add('page-link');
-      pageLinkElement.href = 'javascript:void(0)';
-      pageLinkElement.textContent = item;
-
-      if (item !== currentPage && item !== '...') {
-        pageLinkElement.addEventListener('click', () => observeCard(item));
-      }
-
-      pageItemElement.appendChild(pageLinkElement);
-      numbers.appendChild(pageItemElement);
-
-      if (item === currentPage) {
-        pageItemElement.classList.add('active');
-      }
-    });
-
-    prevPage.classList.toggle('disable', currentPage === 1);
-    firstPage.classList.toggle('disable', currentPage === 1);
-    nextPage.classList.toggle('disable', currentPage === totalPages);
-    lastPage.classList.toggle('disable', currentPage === totalPages);
-
-    observeCard(currentPage);
+  totalPages = Math.ceil(bookArray.length / perPage);
+  if (pageNumber === 1 || pageNumber > totalPages) {
+    numbersPage.innerHTML = creatButtonPages(totalPages);
   }
 
-  function observeCard(page) {
-    if (page === '...') return;
-    currentPage = page;
-
-    updatePagination();
-
-    const startIndex = (currentPage - 1) * perPage;
-    const endIndex = startIndex + perPage;
-    const visibleBooks = bookArray.slice(startIndex, endIndex);
-
-    quantityCards.innerHTML = '';
-    visibleBooks.forEach(book => {
-      const bookItem = document.createElement('li');
-      bookItem.classList.add('shopping__list__item');
-      bookItem.innerHTML = creatMarkup([book]);
-      quantityCards.appendChild(bookItem);
-    });
+  if (pageNumber === 1) {
+    const firstElement = numbersPage.firstElementChild;
+    firstElement?.classList.add('active-page');
   }
 
-  prevPage.addEventListener('click', () => {
-    if (currentPage > 1) {
-      observeCard(currentPage - 1);
-    }
-  });
+  if (pageNumber > totalPages) {
+    pageNumber -= 1;
+    const lastElement = numbersPage.lastElementChild;
+    lastElement?.classList.add('active-page');
+    setMarkupBooks(perPage);
+  }
+  setMarkupBooks(perPage);
+}
 
-  nextPage.addEventListener('click', () => {
-    const totalPages = Math.ceil(bookArray.length / perPage);
-    if (currentPage < totalPages) {
-      observeCard(currentPage + 1);
-    }
-  });
+window.addEventListener('resize', handleViewportResize);
+window.addEventListener('load', handleViewportResize);
 
-  firstPage.addEventListener('click', () => {
-    if (currentPage !== 1) {
-      observeCard(1);
-    }
-  });
+export function setMarkupBooks(currentItem) {
+  const startIndex = (pageNumber - 1) * currentItem;
+  const endIndex = startIndex + currentItem;
+  const visibleBooks = bookArray.slice(startIndex, endIndex);
 
-  lastPage.addEventListener('click', () => {
-    const totalPages = Math.ceil(bookArray.length / perPage);
-    if (currentPage !== totalPages) {
-      observeCard(totalPages);
-    }
-  });
+  setMarkup(visibleBooks);
+}
 
-  numbers.addEventListener('click', event => {
-    if (event.target.classList.contains('page-link')) {
-      const pageNum = parseInt(event.target.textContent);
-      if (!isNaN(pageNum) && pageNum !== currentPage) {
-        observeCard(pageNum);
-      }
-    }
-  });
+function onClick(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
 
-  updatePagination();
-});
+  const buttonValue = evt.target.textContent;
+  if (buttonValue === '<') {
+    pageNumber -= 1;
+  } else if (buttonValue === '>') {
+    pageNumber += 1;
+  } else if (buttonValue === '<<') {
+    pageNumber = 1;
+  } else if (buttonValue === '>>') {
+    pageNumber = totalPages;
+  } else {
+    pageNumber = Number(buttonValue);
+  }
+
+  if (pageNumber <= 1) {
+    firstPage.disabled = true;
+    prevPage.disabled = true;
+    nextPage.disabled = false;
+    lastPage.disabled = false;
+  } else if (pageNumber >= totalPages) {
+    lastPage.disabled = true;
+    nextPage.disabled = true;
+    prevPage.disabled = false;
+    firstPage.disabled = false;
+  } else if (pageNumber < totalPages || pageNumber > 1) {
+    nextPage.disabled = false;
+    lastPage.disabled = false;
+    prevPage.disabled = false;
+    firstPage.disabled = false;
+  }
+
+  handleViewportResize();
+}
+
+function creatButtonPages(page) {
+  const visiblePages = [];
+  for (let i = 1; i <= page; i += 1) {
+    if (i) {
+      visiblePages.push(i);
+    }
+  }
+  return visiblePages.reduce(
+    (acc, page) =>
+      acc +
+      `<button type="button" class="page-link page-item current-page">${page}</button>`,
+    ''
+  );
+}
+
+function onActiveBtn(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const currentActiveBtn = document.querySelector('.active-page');
+  currentActiveBtn?.classList.remove('active-page');
+
+  const nextActivBtn = evt.target;
+  nextActivBtn.classList.add('active-page');
+}
