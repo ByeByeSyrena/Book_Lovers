091db66ad2f36f9cff4c17e0897a0f60907bef96
Ira S <iryna.shevchenko2@gmail.com>
2023-09-05 00:25:44 +0300

    get 200!!!

--- src/js/fetch-one-book-info.js
+++ src/js/fetch-one-book-info.js
@@ -2,12 +2,8 @@ import axios from 'axios';
 const BASE_URL = `https://books-backend.p.goit.global/books/`
 
 export default function getIdData(bookId) {
-    return axios.get(BASE_URL, {
-        params:
-        {
-            _id: bookId,
-        }
-    })
+    const BASE_URL = `https://books-backend.p.goit.global/books/${bookId}`
+    return axios.get(BASE_URL)
 .then(response => {
     if (response.status !== 200) {
       throw new Error();
