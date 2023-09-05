55978f3919ba71e584dec4dd3bd96e18535f9e63
Vitalina03 <127879557+Vitalina03@users.noreply.github.com>
2023-09-05 18:50:45 +0300

    com 11
    доробила брейк поинти .svg скачала для закриття модалки та вставила ії до розмітки щоб підтягнути

--- src/js/markup-for-one-book.js
+++ src/js/markup-for-one-book.js
@@ -1,3 +1,5 @@
+import svg from 'bundle-text:../images/x-close.svg'
+
 export default function markupForOneBook(book) {
     const buyLinks = book.buy_links || [];
 
@@ -12,7 +14,9 @@ export default function markupForOneBook(book) {
     return `
         <div class="backdrop">
             <div class="modal-window">
-            <button type="button" class="close-modal-button">Close</button>
+            <button type="button" class="close-button"><svg class="close-svg" width="24" height="24">
+            ${svg}
+         </svg></button>
                 <img class="best-books-img" src="${book.book_image}" alt="${book.title}" width="218" height="316">
                 <h3 class="book-title" data-name="${book.title}">${book.title}</h3>
                 <p class="author-bestsellers" data-name="${book.author}">${book.author}</p>
