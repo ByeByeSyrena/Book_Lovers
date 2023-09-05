cd9c296acb4284199dafd61b413312338bc62b6b
Potapenko Anna <potapka23@gmail.com>
2023-09-06 00:08:43 +0300

    switch

--- src/js/theme-light-black.js
+++ src/js/theme-light-black.js
@@ -1,27 +1,39 @@
 const themeSelector = document.querySelector('.toggle');
+const slider = document.querySelector('slider');   
+const themeSwitch = document.getElementById('themeSwitch');
+
 themeSelector.addEventListener('click', onChangeColor);
 
 function onChangeColor(event) {
-        event.preventDefault();
-        if (localStorage.getItem('theme') === 'dark') {
-          localStorage.removeItem('theme');
-        }
-        else {
-          localStorage.setItem('theme', 'dark')
-        }
-        addDarkClassToHTML()
-      };
-      
-      function addDarkClassToHTML() {
-        try {
-          if (localStorage.getItem('theme') === 'dark') {
-            document.querySelector('body').classList.add('dark');
-          }
-          else {
-            document.querySelector('body').classList.remove('dark');
-          }
-        } catch (err) { }
-      }
-      
-      addDarkClassToHTML();
+  event.preventDefault();
+  if (localStorage.getItem('theme') === 'dark') {
+    localStorage.removeItem('theme');
+    themeSwitch.checked = false;  
+  } else {
+    localStorage.setItem('theme', 'dark');
+    themeSwitch.checked = true; 
+  }
+  addDarkClassToHTML();
+}
+
+function addDarkClassToHTML() { 
+  try {
+    if (localStorage.getItem('theme') === 'dark') { 
+      document.querySelector('body').classList.add('dark');
+      slider.style.transform = 'translateX(17px)'; //темна теми
+      slider.style.backgroundColor = ' rgb(50, 40, 93)'; 
+    } else { 
+      document.querySelector('body').classList.remove('dark'); 
+      slider.style.transform = 'translateX(0)'; //світла тема
+      slider.style.backgroundColor = ' rgb(117, 100, 190)'; 
+    }
+  } catch (err) {}
+}  
+
+addDarkClassToHTML();
+
+
+
+
+
 
