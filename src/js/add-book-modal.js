89c3fbe8893910ce2957e00e994b3aec30bc1d4a
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-09-05 17:29:03 +0300

    css markup modal

--- src/js/add-book-modal.js
+++ src/js/add-book-modal.js
@@ -1,23 +1,34 @@
-import getIdData from "./fetch-one-book-info";
-import markupForOneBook from "./markup-for-one-book";
-
+import getIdData from './fetch-one-book-info';
+import markupForOneBook from './markup-for-one-book';
 
 const bookRefs = {
-    selector: document.querySelector(".main-books-container"),
-}
+  selector: document.querySelector('.main-books-container'),
+};
 
 const { selector } = bookRefs;
 
-selector.addEventListener("click", showBookInfo);
+selector.addEventListener('click', showBookInfo);
 
-function showBookInfo(event){
-    const bookItem = event.target.closest('.book-item');
-    if (bookItem) {
-        const bookId = bookItem.id;
-        getIdData(bookId)
-            .then(data => {
-                markupForOneBook(data);
-            })
-            .catch(error => console.log(error.message));
-    }
+function showBookInfo(event) {
+  const bookItem = event.target.closest('.book-item');
+  if (bookItem) {
+    const bookId = bookItem.id;
+    getIdData(bookId)
+      .then(data => {
+        markupForOneBook(data);
+      })
+      .catch(error => console.log(error.message));
+  }
+
+//   const modal = document.querySelector('.modal-windoww');
+
+//   const clickX = event.clientX + window.scrollX;
+//   const clickY = event.clientY + window.scrollY;
+
+//   modal.style.left = clickX + 'px';
+//   modal.style.top = clickY + 'px';
+
+//   modal.classList.add('show-modal'); 
+
+//   event.preventDefault();
 }
