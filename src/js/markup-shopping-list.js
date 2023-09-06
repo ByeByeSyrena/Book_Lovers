b285a86a032f25001b7436bfd1c3c841e901dadf
Andrey <andrey291188@gmail.com>
2023-09-06 21:18:27 +0300

    last)))

--- src/js/markup-shopping-list.js
+++ src/js/markup-shopping-list.js
@@ -2,122 +2,9 @@ import { handleViewportResize } from './shopping-list';
 
 const BOOKS_CARDS = 'bookCards';
 
-const arraTest = [
-  {
-    id: '1',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 1',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '2',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 2',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '3',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 3',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '4',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 4',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '5',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 5',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '6',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 6',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '7',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 7',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '8',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 8',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '9',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 9',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '10',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 10',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-  {
-    id: '11',
-    img: 'https://netsh.pp.ua/wp-content/uploads/2017/08/Placeholder-1.png',
-    alt: 'book images',
-    title: 'Book title 11',
-    category: 'categorya',
-    description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Provident ducimus sunt nostrum esse enim obcaecati cupiditate est veniam assumenda neque ea aut quam facilis deserunt laboriosam incidunt nemo, alias ex',
-    author: 'book author',
-  },
-];
-
-localStorage.setItem(BOOKS_CARDS, JSON.stringify(arraTest));
 
 export let bookArray = JSON.parse(localStorage.getItem(BOOKS_CARDS));
+
 
 const listMarkup = document.querySelector('.js-markup-shopping__list');
 const placeholder = document.querySelector('.js-placeholder');
@@ -126,12 +13,13 @@ const pagination = document.querySelector('.pagination');
 listMarkup.addEventListener('click', deleteCard);
 
 export function deleteCard(evt) {
-  evt.preventDefault();
+
   if (!evt.target.id) {
     return;
   }
   const bookIdDelete = evt.target.id;
-  const newArray = bookArray.filter(book => book.id !== bookIdDelete);
+  
+  const newArray = bookArray.filter(book => book._id !== bookIdDelete);
 
   localStorage.setItem(BOOKS_CARDS, JSON.stringify(newArray));
   bookArray = JSON.parse(localStorage.getItem(BOOKS_CARDS));
@@ -154,45 +42,45 @@ export function setMarkup(array) {
 
 export function creatMarkup(books) {
   return books.reduce(
-    (acc, { id, img, alt, title, category, description, author }) =>
+    (acc, { _id, book_image, title, list_name, description, author, buy_links }) =>
       acc +
       `<li class="shopping__list__item ">
-        <button type="button" class="shopping__list__btn__delete delete__button" id="${id}">
+        <button type="button" class="shopping__list__btn__delete delete__button" id="${_id}">
         </button>
         <img
           class="shopping__list__book__image"
-          src="${img}"
-          alt="${alt}"
+          src="${book_image}"
+          alt="${title}"
         />
         <div class="shopping__list__box__description">
           <h3 class="shopping__list__book__title">${title}</h3>
-          <p class="shopping__list__category">${category}</p>
-          <p class="shopping__list__description">${description}</p>
+          <p class="shopping__list__category">${list_name}</p>
+          <p class="shopping__list__description">${description? description : "Sorry no description"}</p>
           <p class="shopping__list__author">${author}</p>
         </div>
         <ul class="shopping__list__link__list">
         <li class="shopping__list__link__item">
-          <a href=""
+          <a href="${buy_links[0].url}" target="_blank"
             ><img
               class="shopping__list__link__images amazon"
               src="${require('../images/shopping-list/link-svg/image1.svg')}"
-              alt="link books amazon"
+              alt="${buy_links[0].name}"
           /></a>
         </li>
         <li class="shopping__list__link__item">
-          <a href=""
+          <a href="${buy_links[1].url}" target="_blank"
             ><img
               class="shopping__list__link__images"
               src="${require('../images/shopping-list/link-svg/image2.svg')}"
-              alt="link books"
+              alt="${buy_links[1].url}"
           /></a>
         </li>
         <li class="shopping__list__link__item">
-          <a href=""
+          <a href="${buy_links[4].url}" target="_blank"
             ><img
               class="shopping__list__link__images"
               src="${require('../images/shopping-list/link-svg/image3.svg')}"
-              alt="link books"
+              alt="${buy_links[4].name}"
           /></a>
         </li>
       </ul>
