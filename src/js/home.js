f9f00ca5d6f26c07e5e8fe73887e7b129c2ae0e4
Ira S <iryna.shevchenko2@gmail.com>
2023-09-06 11:50:40 +0300

    update

--- src/js/home.js
+++ src/js/home.js
@@ -56,42 +56,34 @@ function getBestSellers() {
 
         if (window.innerWidth < 768) {
             bestSellersContainer.innerHTML = markupTopBooks(booksData);
-
-            const bookItems = document.querySelectorAll(".book-item");
-            bookItems.forEach(item => {
-                item.addEventListener("click", getBookCard);
-            });
         } else if (window.innerWidth >= 768 && window.innerWidth <= 1440) {
             bestSellersContainer.innerHTML = markupForTabletOfTopBooks(booksData);
-            
-            const bookItems = document.querySelectorAll(".book-item");
-            bookItems.forEach(item => {
-                item.addEventListener("click", getBookCard);
-            });
         } else if (window.innerWidth > 1440) {
             bestSellersContainer.innerHTML = markupForDesktopOfTopBooks(booksData);
+        }
 
-            const bookItems = document.querySelectorAll(".book-item");
-            bookItems.forEach(item => {
-                item.addEventListener("click", getBookCard);
-            });
-        }
-        
-        function getBookCard(event) {
-            const selectedBookElement = event.target.closest('.book-item');
-            if (selectedBookElement) {
-                const selectedBookId = selectedBookElement.dataset.id;
-                getIdData(selectedBookId)
-                    .then(data => {
-                        const modalMarkup = markupForOneBook(data);
-                const modalContainer = document.getElementById('modal-container');
-                modalContainer.innerHTML = modalMarkup;
-                    })
-                    .catch(error => console.log(error.message));
-            }
-        }
+        const bookItems = document.querySelectorAll(".book-item");
+        bookItems.forEach(item => {
+            item.addEventListener("click", getBookCard);
+        });
     }).catch(error => console.log(error));
 }
+
+function getBookCard(event) {
+    const selectedBookElement = event.target.closest('.book-item');
+    if (selectedBookElement) {
+        const selectedBookId = selectedBookElement.dataset.id;
+        getIdData(selectedBookId)
+            .then(data => {
+                const modalMarkup = markupForOneBook(data);
+                const modalContainer = document.getElementById('modal-container');
+                modalContainer.innerHTML = modalMarkup;
+            })
+            .catch(error => console.log(error.message));
+    }
+}
+
+getBestSellers();
 
 
 // selector.addEventListener('click', showBookInfo);
