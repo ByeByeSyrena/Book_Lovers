d130ebf1bcda4014194975872e2d1601c406e94d
Username-Bogdan <127837806+Bogdan-Go-It@users.noreply.github.com>
2023-09-06 13:39:47 +0300

    modalfin1

--- src/js/home.js
+++ src/js/home.js
@@ -1,98 +1,97 @@
-import getAllCategories from "./fetch-category-list";
+import getAllCategories from './fetch-category-list';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import markupCategoryList from "./markup-category-list";
-import getCategoryData from "./fetch-one-category";
-import getTopBooks from "./fetch-best-sellers";
-import markupByCategory from "./markup-category";
-import markupTopBooks from "./markup-for-best-sellers";
-import markupForTabletOfTopBooks from "./markup-for-tablet";
-import markupForDesktopOfTopBooks from "./markup-for-desktop";
-import markupForOneBook from "./markup-for-one-book";
-import getIdData from "./fetch-one-book-info";
+import markupCategoryList from './markup-category-list';
+import getCategoryData from './fetch-one-category';
+import getTopBooks from './fetch-best-sellers';
+import markupByCategory from './markup-category';
+import markupTopBooks from './markup-for-best-sellers';
+import markupForTabletOfTopBooks from './markup-for-tablet';
+import markupForDesktopOfTopBooks from './markup-for-desktop';
+import markupForOneBook from './markup-for-one-book';
+import getIdData from './fetch-one-book-info';
 
-const listCategories = document.querySelector(".categories-list")
-const mainTitleEl = document.querySelector(".main-title")
-const bestSellersContainer = document.querySelector(".main-books-container");
-let listEl
+const listCategories = document.querySelector('.categories-list');
+const mainTitleEl = document.querySelector('.main-title');
+const bestSellersContainer = document.querySelector('.main-books-container');
+let listEl;
 
 getAllCategories()
-    .then(allCategories => {
-      allCategories.map(categoryName => markupCategoryList(categoryName))
-       listEl = document.querySelectorAll(".item-category")
-   })
-   .catch(() => Notify.failure('Sorry, please reload the page'))
+  .then(allCategories => {
+    allCategories.map(categoryName => markupCategoryList(categoryName));
+    listEl = document.querySelectorAll('.item-category');
+  })
+  .catch(() => Notify.failure('Sorry, please reload the page'));
 
-listCategories.addEventListener("click", onClickCategory)
+listCategories.addEventListener('click', onClickCategory);
 
 getBestSellers();
 
 function onClickCategory(e) {
-   const activeLiEl = e.target;
-   const nameCategory = activeLiEl.dataset.name;
-   listEl.forEach(li => {
-       li.classList.remove("active")
-   })
+  const activeLiEl = e.target;
+  const nameCategory = activeLiEl.dataset.name;
+  listEl.forEach(li => {
+    li.classList.remove('active');
+  });
 
-    activeLiEl.classList.add("active")
-    bestSellersContainer.innerHTML = "";
+  activeLiEl.classList.add('active');
+  bestSellersContainer.innerHTML = '';
 
-   if (nameCategory) {
-     mainTitleEl.innerHTML = nameCategory;
-     getCategoryData(nameCategory)
-       .then(booksData => {
-           booksData.map(book => markupByCategory(book))
-       })
-       .catch(error => console.log(error))
-
-   } else {
-       getBestSellers();
-  
-   }
+  if (nameCategory) {
+    mainTitleEl.innerHTML = nameCategory;
+    getCategoryData(nameCategory)
+      .then(booksData => {
+        booksData.map(book => markupByCategory(book));
+      })
+      .catch(error => console.log(error));
+  } else {
+    getBestSellers();
+  }
 }
 
 function getBestSellers() {
-    getTopBooks().then(booksData => {
-        mainTitleEl.innerHTML = 'Best Sellers <span class="colored">Books</span>';
+  getTopBooks()
+    .then(booksData => {
+      mainTitleEl.innerHTML = 'Best Sellers <span class="colored">Books</span>';
 
-        if (window.innerWidth < 768) {
-            bestSellersContainer.innerHTML = markupTopBooks(booksData);
+      if (window.innerWidth < 768) {
+        bestSellersContainer.innerHTML = markupTopBooks(booksData);
 
-            const bookItems = document.querySelectorAll(".book-item");
-            bookItems.forEach(item => {
-                item.addEventListener("click", getBookCard);
-            });
-        } else if (window.innerWidth >= 768 && window.innerWidth <= 1440) {
-            bestSellersContainer.innerHTML = markupForTabletOfTopBooks(booksData);
-            
-            const bookItems = document.querySelectorAll(".book-item");
-            bookItems.forEach(item => {
-                item.addEventListener("click", getBookCard);
-            });
-        } else if (window.innerWidth > 1440) {
-            bestSellersContainer.innerHTML = markupForDesktopOfTopBooks(booksData);
+        const bookItems = document.querySelectorAll('.book-item');
+        bookItems.forEach(item => {
+          item.addEventListener('click', getBookCard);
+        });
+      } else if (window.innerWidth >= 768 && window.innerWidth <= 1440) {
+        bestSellersContainer.innerHTML = markupForTabletOfTopBooks(booksData);
 
-            const bookItems = document.querySelectorAll(".book-item");
-            bookItems.forEach(item => {
-                item.addEventListener("click", getBookCard);
-            });
+        const bookItems = document.querySelectorAll('.book-item');
+        bookItems.forEach(item => {
+          item.addEventListener('click', getBookCard);
+        });
+      } else if (window.innerWidth > 1440) {
+        bestSellersContainer.innerHTML = markupForDesktopOfTopBooks(booksData);
+
+        const bookItems = document.querySelectorAll('.book-item');
+        bookItems.forEach(item => {
+          item.addEventListener('click', getBookCard);
+        });
+      }
+
+      function getBookCard(event) {
+        const selectedBookElement = event.target.closest('.book-item');
+        if (selectedBookElement) {
+          const selectedBookId = selectedBookElement.dataset.id;
+          getIdData(selectedBookId)
+            .then(data => {
+              const modalMarkup = markupForOneBook(data);
+              const modalContainer = document.getElementById('modal-container');
+              modalContainer.innerHTML = modalMarkup;
+            })
+            .catch(error => console.log(error.message));
         }
-        
-        function getBookCard(event) {
-            const selectedBookElement = event.target.closest('.book-item');
-            if (selectedBookElement) {
-                const selectedBookId = selectedBookElement.dataset.id;
-                getIdData(selectedBookId)
-                    .then(data => {
-                        const modalMarkup = markupForOneBook(data);
-                const modalContainer = document.getElementById('modal-container');
-                modalContainer.innerHTML = modalMarkup;
-                    })
-                    .catch(error => console.log(error.message));
-            }
-        }
-    }).catch(error => console.log(error));
+      }
+    })
+    .catch(error => console.log(error));
 }
-
 
 // selector.addEventListener('click', showBookInfo);
 
--- src/js/markup-for-one-book.js
+++ src/js/markup-for-one-book.js
@@ -1,40 +1,166 @@
-export default function markupForOneBook({book_image, buy_links, author, description, title}) {
-    const buyLinks = buy_links || [];
-    const sliceArray = buyLinks.slice(0, 3);
+import getIdData from './fetch-one-book-info';
+// const iDBOOK = '';
 
-    const buyLinksHTML = sliceArray.map(({url, name}) => `
-        <li class="">
-            <a href="${url}" target="_blank">
-                <img class="shopping__list__link__images" src="${getLinkImageSource(name)}" alt="link books">
-            </a>
-        </li>
-    `).join('');
+// (() => {
+//   const refs = {
+//     // openModalBtn: document.querySelector('.btn-book'),
+//     closeModalBtn: document.querySelector('[data-modal-pop-up-close]'),
+//     modal: document.querySelector('[data-modal-pop-up]'),
+//   };
+//   //   console.log(refs.openModalBtn);
+//   //   refs.openModalBtn.addEventListener('click', toggleModal);
 
-    return `
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+//   window.addEventListener('keydown', e => {
+//     if (e.key === 'Escape') {
+//       toggleModal();
+//     }
+//   });
+//   document
+//     .querySelector('.pop-up .pop-up-container')
+//     .addEventListener('click', event => {
+//       event._isClicWithInModal = true;
+//     });
+//   refs.modal.addEventListener('click', event => {
+//     if (event._isClicWithInModal) return;
+//     toggleModal();
+//   });
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+const items = JSON.parse(localStorage.getItem('bookCards')) || [];
+export default function markupForOneBook({
+  _id,
+  book_image,
+  buy_links,
+  author,
+  description,
+  title,
+}) {
+  //   iDBOOK = _id;
+  const buyLinks = buy_links || [];
+  const sliceArray = buyLinks.slice(0, 3);
+
+  const buyLinksHTML = sliceArray
+    .map(
+      ({ url, name }) => `
+      <li class="">
+          <a href="${url}" target="_blank">
+              <img class="shopping__list__link__images" src="${getLinkImageSource(
+                name
+              )}" alt="link books">
+          </a>
+      </li>
+  `
+    )
+    .join('');
+  getIdData(_id).then(content => {
+    const umper = document.querySelector('[pop-up-umper]');
+    const localStorBtn = document.querySelector('.yourorder-modal-submit-btn');
+    localStorBtn.addEventListener('click', localStorageChange);
+
+    let status = 'add';
+
+    function localStorageChange() {
+      // for (const key in items) {
+      //   if (items[key]. === content) {
+      //     console.log(1);
+      //   } else {
+      //     console.log(2);
+      //   }
+      // }
+
+      if (items.find(option => option._id === content._id)) {
+        console.log(1);
+        status = 'add';
+        localStorBtn.textContent = 'remove from the shopping list';
+        umper.removeAttribute('is-hidden');
+
+        const deletable = content._id;
+
+        for (let i = 0, len = items.length; i < len; i++) {
+          if (items[i]._id === deletable) {
+            items.splice(i, 1);
+            break;
+          }
+        }
+
+        // items.pop(content);
+        localStorage.setItem('bookCards', JSON.stringify(items));
+        return;
+      } else {
+        console.log(2);
+        localStorBtn.textContent = ' add to shopping list';
+        umper.classList.add('is-hidden');
+
+        items.push(content);
+        localStorage.setItem('bookCards', JSON.stringify(items));
+
+        return;
+      }
+    }
+    console.log(content);
+  });
+  console.log(_id);
+  return `
         <div class="backdrop">
             <div class="modal-window">
-            <button type="button" class="close-modal-button">Close</button>
-                <img class="best-books-img" src="${book_image}" alt="${title}" width="218" height="316">
-                <h3 class="book-title" data-name="${title}">${title}</h3>
-                <p class="author-bestsellers" data-name="${author}">${author}</p>
-                <p class="descriptions" data-name="${description}">${description}</p>
-                <div class="buy-links">
-                    <ul class="">
-                        ${buyLinksHTML}
+            <button
+          class="yourorder-modal-close-btn btn"
+          type="button"
+          data-modal-pop-up-close
+        >
+          <svg class="yourorder-modal-icon-close" width="12" height="12">
+            <use href="./sprite.svg#icon-x"></use>
+          </svg>
+        </button>
+
+        <ul class="pop-up__list__list">
+          <li class="popup__list__item">
+                <img
+                  class="popup__list__book__image"
+                  src="${book_image}"
+                  alt="${title}"
+                />
+                <div class="popup__list__box__description">
+                  <h3 class="poup__list__book__title" data-name="${title}">${title}</h3>
+                  <p class="popup__list__author" data-name="${author}">${author}</p>
+                  <p class="popup__list__description" data-name="${description}">${description}</p>
+<ul class="">
+                         ${buyLinksHTML}
                     </ul>
+
                 </div>
-                <button type="button" class="add-to-shopping-list">Add to shopping list</button>
+
+              </li>
+          <li class="box-modal-btn">
+            <button class="yourorder-modal-submit-btn" type="button">
+              Add to shopping list
+            </button>
+            <div class="umper-form is-hidden" pop-up-umper>
+              <p class="umpertitle-pop">
+                Сongratulations! You have added the book to the shopping list.
+                To delete, press the button “Remove from the shopping list”.
+              </p>
+            </div>
+          </li>
+        </ul>
             </div>
         </div>
     `;
 }
 
 function getLinkImageSource(linkName) {
-    const linkImageMappings = {
-        "Amazon": require('../images/shopping-list/link-svg/image1.svg'),
-        "Apple Books": require('../images/shopping-list/link-svg/image2.svg'),
-        "Barnes and Noble": require('../images/shopping-list/link-svg/image3.svg'),
-    };
+  const linkImageMappings = {
+    Amazon: require('../images/shopping-list/link-svg/image1.svg'),
+    'Apple Books': require('../images/shopping-list/link-svg/image2.svg'),
+    'Barnes and Noble': require('../images/shopping-list/link-svg/image3.svg'),
+  };
 
-    return linkImageMappings[linkName] || '';
+  return linkImageMappings[linkName] || '';
 }
+// getIdData().then(cntent => {
+//   console.log(content);
+// });
+// console.log(iDBOOK);
