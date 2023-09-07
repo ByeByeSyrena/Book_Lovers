e8a31ae717177116268274317f2c9f0109c01238
Ira S <iryna.shevchenko2@gmail.com>
2023-09-07 09:24:01 +0300

    header menu update

--- src/js/header.js
+++ src/js/header.js
@@ -38,22 +38,40 @@ const pageLinks = document.querySelectorAll('menu-link');
 }); 
 
 
-// Header navigation - current location
+// Header navigation - current location -- допрацьовую!!!
+function colorCurrentPage() {
+  const currentPageURL = window.location.href;
 
-function colorCurrentPage(){
-const currentPageURL = window.location.href;
+  const homeLinkLi = document.querySelector(".li-to-home-page");
+  const shoppingListLinkLi = document.querySelector(".li-to-shopping-page");
 
-const homeLinkLi = document.querySelector(".li-to-home-page");
-const shoppingListLinkLi = document.querySelector(".li-to-shopping-page");
 
-if (currentPageURL.includes("index.html")) {
-  homeLinkLi.classList.add("change-background"); 
-} else if (currentPageURL.includes("shopping-list.html")) {
-  shoppingListLinkLi.classList.add("change-background");
+  const homeLinkMobile = document.querySelector(".mobile-menu-home-button");
+  const shoppingLinkMobile = document.querySelector(".mobile-menu-shopping-list");
+
+  const body = document.body;
+
+  if (currentPageURL.includes("index.html")) {
+    homeLinkLi.classList.add("change-background");
+    homeLinkMobile.classList.add("change-background");
+
+    shoppingListLinkLi.classList.remove("change-background");
+    // shoppingLinkMobile.classList.remove("change-background");
+
+  } else if (currentPageURL.includes("shopping-list.html")) {
+    shoppingListLinkLi.classList.add("change-background");
+    // shoppingLinkMobile.classList.add("change-background");
+
+    homeLinkLi.classList.remove("change-background");
+    // homeLinkMobile.classList.remove("change-background");
+  }
 }
 
-}
 colorCurrentPage();
+
+
+
+
 
  
 
